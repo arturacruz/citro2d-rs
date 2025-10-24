515035ad0398a9aa3bd9356961fa51e73f2a517c
Artur Cruz <arturacruz@icloud.com>
2025-10-24 00:30:35 -0300

    finally got it working

--- citro2d/src/render.rs
+++ citro2d/src/render.rs
@@ -2,24 +2,32 @@ use std::ptr::NonNull;
 
 use citro2d_sys::{C2D_CreateScreenTarget, C3D_RenderTarget};
 
-pub struct RenderTarget {
+pub struct Target {
     ptr: NonNull<C3D_RenderTarget>
-
 }
 
-pub enum Screen {
+pub enum TargetError {
+    Init2D,
+}
+
+pub enum Screen { 
     TopLeft, TopRight, Bottom
 }
 
 
-impl RenderTarget {
-    pub fn new(screen: Screen) {
+impl Target {
+    pub fn new(screen: Screen) -> Result<Target, TargetError> {
         let (screen, side) = match screen {
             Screen::TopLeft => (0, 0),
             Screen::TopRight => (0, 1),
-            Screen::Bottom => (1, 0)
+            Screen::Bottom => (1, 0)  
         };
     
-        let a = unsafe { C2D_CreateScreenTarget(screen, side) };
+        let ptr = match NonNull::new(unsafe { C2D_CreateScreenTarget(screen, side) }) {
+            Some(p) => p,
+            None => return Err(TargetError::Init2D)
+        };
+
+        Ok(Target { ptr })
     }
 }
